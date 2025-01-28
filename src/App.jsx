9ed6a7f7a703ac52f433b613fb540e92809b9a41
İsmail Efe Ataş <96317271+efe-atas@users.noreply.github.com>
2025-01-28 21:36:43 +0300

    Lectures updated.

--- src/App.jsx
+++ src/App.jsx
@@ -976,7 +976,7 @@ function App() {
         color: theme.colors.text.secondary,
         fontStyle: 'italic'
       }}>
-        Son güncelleme: 24/01/2025
+        Son güncelleme: 28/01/2025
       </Typography>
 
       {/* Header Section */}
